7d181ebf0d3aa0c153e4165ab08d44a6af418ba7
Nikhil <nikhil.mittal@iimjobs.com>
2026-02-01 20:15:13 +0530

    Api setup

--- src/constants/APIConstants.js
+++ src/constants/APIConstants.js
@@ -1,4 +1,5 @@
 export const RECRUITER_ENDPOINTS = {
-    SEARCH_BY_ID: (id) => `/recruiters/${id}`,
-    LIST: "/recruiters",
+    SEARCH_BY_ID: (id) => `/recruiter/${id}`,
+    LIST: "/recruiter",
+    SEARCH_BY_DOMAIN: (domain) => `/recruiter/domain/${domain}`,
 };
